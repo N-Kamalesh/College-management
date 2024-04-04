553244a6904eea9948a390588b0bae4c81b1e084
n-kamalesh <kamaltvt579@gmail.com>
2024-04-04 17:03:04 +0530

    Complete initial setup of front-end

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-red-500 flex text-center h-screen w-screen justify-center items-center">
+      App
+    </div>
+  );
+}
+
+export default App;
